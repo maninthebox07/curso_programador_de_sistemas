b4c47f59b61c355c609db569b127ee1db29558a3
Renan Maziero <renanmaziero07@gmail.com>
2024-08-28 22:00:04 -0300

    Resolução do exercício 19 da lista #4

--- Exercicios/JavaScript/Exercicios4/verificarAnagrama.js
+++ Exercicios/JavaScript/Exercicios4/verificarAnagrama.js
@@ -1,6 +1,11 @@
 // 19. Desafio Verifique se uma string é um anagrama de outra.
 function verificarAnagrama(str, str2) {
-	
+	if(str.length != str2.length) return false;
+
+    let a = str.split('').sort().join('');
+    let b = str2.split('').sort().join('');
+
+    return a == b ? true : false;
 }
 // C1
 console.log(verificarAnagrama("listen", "silent")); // true
