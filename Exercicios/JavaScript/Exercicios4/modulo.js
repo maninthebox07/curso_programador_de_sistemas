e9fc8a02e105d9b6ba99e4daee1436411866b531
Renan <renanmaziero07@gmail.com>
2024-08-29 14:22:46 -0300

    Cria um módulo para o exercício 10 da lista #4

---
+++ Exercicios/JavaScript/Exercicios4/modulo.js
@@ -0,0 +1,9 @@
+function somaPares(array) {
+	let arrayPar = array.filter(value => value % 2 == 0)
+
+	const initValue = 0;
+	
+	return arrayPar.reduce((accum, curr) => accum + curr, initValue);
+}
+
+module.exports = somaPares;
