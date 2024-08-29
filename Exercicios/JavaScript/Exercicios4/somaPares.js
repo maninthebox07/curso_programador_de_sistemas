4e5a49d8625d7119e3f50422915e2bb34b8f116e
Renan <renanmaziero07@gmail.com>
2024-08-29 14:55:31 -0300

    Readequação nos cenários de teste do exercício 10 lista #4.

--- Exercicios/JavaScript/Exercicios4/somaPares.js
+++ Exercicios/JavaScript/Exercicios4/somaPares.js
@@ -1,8 +1,8 @@
 // 10. Calcule a soma dos números pares de um array.
 const somaPares = require("./modulo");
 // C1
-console.log(somaPares([1, 2, 3, 4, 5])); // 6
+console.log(somaPares([1, 2, 3, 4, 5]) == 6); // 6
 // C2
-console.log(somaPares([2, 4, 6, 8])); // 20
+console.log(somaPares([2, 4, 6, 8]) == 20); // 20
 // C3
-console.log(somaPares([1, 3, 5, 7])); // 0
+console.log(somaPares([1, 3, 5, 7]) == 0); // 0
