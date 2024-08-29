3b20706c878cc28991144872a7fd75e0347f0912
Renan <renanmaziero07@gmail.com>
2024-08-29 15:19:13 -0300

    Alteração no operador de igualdade nos casos de teste

--- Exercicios/JavaScript/Exercicios4/somaArray.js
+++ Exercicios/JavaScript/Exercicios4/somaArray.js
@@ -2,12 +2,12 @@
 const {somaArray} = require("./modulo");
 
 // C1
-console.log(somaArray([1, 2, 3, 4, 5]) == 15); // 15
+console.log(somaArray([1, 2, 3, 4, 5]) === 15); // 15
 // C2
-console.log(somaArray([2, 4, 6, 8]) == 20); // 20
+console.log(somaArray([2, 4, 6, 8]) === 20); // 20
 // C3
-console.log(somaArray([1, 3, 5, 7]) == 16); // 16
+console.log(somaArray([1, 3, 5, 7]) === 16); // 16
 // C4
-console.log(somaArray([0]) == 0); // 0
+console.log(somaArray([0]) === 0); // 0
 // C5
-console.log(somaArray([]) == 0); // 0
+console.log(somaArray([]) === 0); // 0
